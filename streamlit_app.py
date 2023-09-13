bdcd717d18882ade416fba15dc651081c9f0dd1c
Eugene <57861661+engureguo@users.noreply.github.com>
2023-09-13 17:15:19 +0800

    fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import base64
+
 
 # Page title
 st.title("Base64 Encoder/Decoder")
@@ -11,10 +13,12 @@ input_data = st.text_input("Enter the string to be encoded/decoded")
 
 # Encoding button
 if st.button("Encode"):
-    encoded_data = input_data.encode('base64', 'strict')
+    encoded_data = base64.b64encode(bytes(input_data, 'utf-8'))
+    #encoded_data = input_data.encode('base64', 'strict')
     st.write(encoded_data)
 
 # Decoding button
 if st.button("Decode"):
-    decoded_data = input_data.decode('base64', 'strict')
+    decoded_data = base64.b64decode(bytes(input_data, 'utf-8'))
+    #decoded_data = input_data.decode('base64', 'strict')
     st.write(decoded_data)
